c5f3cf11ee2beafc0f2c9ed63b28e9f4c2400e23
Ariela Cohen <arielaades@gmail.com>
2019-07-29 14:29:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,6 +11,7 @@ def reverse_each_word(sentence)
 sentence_array = sentence.split
 sentence_array.collect do |word|
   word.reverse
+  return sentence_array.join(" ")
   end
-return sentence_array.join(" ")
+
 end
