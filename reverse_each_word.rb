c62d9ebd29e97f8b33e5f6b1bd67ed579ab64e19
Ariela Cohen <arielaades@gmail.com>
2019-07-29 14:03:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,8 @@
 def reverse_each_word(sentence)
-  sentence_array = sentence.split(" ")
+  sentence_array = sentence.split
   reverse_sentence = ""
   sentence_array.each do |word|
-    reverse_sentence << "#{word.reverse}"
+    reverse_sentence += "#{word.reverse}"
   end
   return reverse_sentence
 end
