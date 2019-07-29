50c531f8e6563663c2966157e8019eeee384f537
Ariela Cohen <arielaades@gmail.com>
2019-07-29 14:34:58 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -9,9 +9,8 @@
 
 def reverse_each_word(sentence)
 sentence_array = sentence.split
-sentence_array.collect do |word|
+new_array = sentence_array.collect do |word|
   word.reverse
-
   end
-
+new_array.join
 end
